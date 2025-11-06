ddb463c2040b14e7576ae0d02fcc81235dc5773c
Connor-Levi <184224862+Connor-Levi@users.noreply.github.com>
2025-11-06 10:07:20 +0530

    integrated light interaction

--- objects.py
+++ objects.py
@@ -1,6 +1,13 @@
 import cupy as cp
 from util import normalize
 
+class light():
+    def __init__(self, direction):
+        self.direction = normalize(cp.array(direction, dtype = cp.float32))
+
+    def rays(self):
+            return self.direction
+            
 class sphere:
     def __init__(self, centre, radius, color):
 
@@ -18,7 +25,7 @@ class sphere:
         ex = cp.array([0.01, 0, 0])
         ey = cp.array([0, 0.01, 0])
         ez = cp.array([0, 0, 0.01])
-
+        
         dx = cp.linalg.norm(point + ex, axis = -1) - cp.linalg.norm(point, axis = -1)
         dy = cp.linalg.norm(point + ey, axis = -1) - cp.linalg.norm(point, axis = -1)
         dz = cp.linalg.norm(point + ez, axis = -1) - cp.linalg.norm(point, axis = -1)
@@ -26,10 +33,22 @@ class sphere:
         gradient = cp.stack((dx, dy, dz), axis=-1)
 
         return normalize(gradient)
+    
+    def hitpixels(self, eye, steps, epsilon):
+        rays = eye.rays()
+        pixels = cp.broadcast_to(eye.pos, rays.shape).copy()
+    
+        hitbox = cp.zeros((eye.height, eye.width), dtype = cp.float32)
 
-class light():
-    def __init__(self, direction):
-        self.direction = normalize(cp.array(direction, dtype = cp.float32))
+        for i in range(steps):
+            d = self.distance(pixels)
 
-    def rays(self):
-            return self.direction
+            hit = d < epsilon
+            hitbox = (hitbox + hit) > 0
+
+            d = cp.where(hit, 0, d)
+            pixels = pixels + rays * d[:, :, None]
+
+        return self.normal(pixels[hitbox]), hitbox
+
+
