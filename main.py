845c25bb1d45e4d24953a999117423e1f98b6903
Connor-Levi <184224862+Connor-Levi@users.noreply.github.com>
2025-11-24 13:02:57 +0530

    QoL changes, sphere color changes, resolution fix

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from objects import sphere, light
 from util import blackhole
 
 bh = True
+distortion = 150
 
 height = 1080
-width = 1980
+width = 1920
 epsilon = 0.5
 steps = 5
 
@@ -21,23 +22,23 @@ eye = camera(pos = [0, 0, 0],
 
 ball = sphere(centre = [10, 0, 0],
         radius = 3, 
-        color = [125, 90, 170])
+        color = [114, 135, 253])
 
 ball1 = sphere(centre = [5, 0, 2],
         radius = 0.7, 
-        color = [120, 55, 55])
+        color = [230, 69, 83])
 
 photon = light(direction = [0, 1, 1])
 
 scene = scene(eye, [ball, ball1], [photon])
 
-pic = scene.render(steps, epsilon)
+pic = scene.render(steps, epsilon, width, height)
 
 bx = width / 2
 by = height / 2
 
 if bh == True:
-    distorted = blackhole(pic, bx, by, 50)
+    distorted = blackhole(pic, bx, by, distortion)
     img = Image.fromarray(cp.asnumpy(distorted))
 
 else:
